76cfea9e453564387b564aacbb6aba79f8e1e498
takko <ameato9ei@gmail.com>
2026-01-12 17:15:14 +0900

    glasssglobe: optimized the off-screen render resolution, enhance transparent quality.

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 // Add controller for debugging
